e277d4995542acf65a50e08671b1874c48adc23b
keiji.yoshida@databricks.com <keiji.yoshida@databricks.com>
2021-02-16 11:22:34 +0000

    Add Spark SQL Join Strategies.py

---
+++ Spark SQL Join Strategies.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Spark SQL Join Strategies
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Databricks Runtime Version: 7.6 ML (includes Apache Spark 3.0.1, Scala 2.12)
+
+# COMMAND ----------
+
