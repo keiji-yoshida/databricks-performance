89a88abe6bdd41e6ceeb1166b9d359d29a782e65
keiji.yoshida@databricks.com <keiji.yoshida@databricks.com>
2021-02-16 11:51:23 +0000

    Update files

--- Spark SQL Join Strategies/Spark SQL Join Strategies.py
+++ Spark SQL Join Strategies/Spark SQL Join Strategies.py
@@ -1,1 +1,21 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Spark SQL Join Strategies
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Databricks Runtime Version: 7.6 ML (includes Apache Spark 3.0.1, Scala 2.12)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Resources
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC * [Join Strategy Hints for SQL Queries | Performance Tuning - Spark 3.0.1 Documentation](https://spark.apache.org/docs/latest/sql-performance-tuning.html#join-strategy-hints-for-sql-queries)
+# MAGIC * [Join Hints | Hints - Spark 3.0.1 Documentation](https://spark.apache.org/docs/latest/sql-ref-syntax-qry-select-hints.html#join-hints)
+# MAGIC * [Tech Talk: Top Tuning Tips for Spark 3.0 and Delta Lake on Databricks - YouTube](https://www.youtube.com/watch?v=hcoMHnTcvmg)
+# MAGIC * [Optimizing Apache Spark SQL Joins - Databricks](https://databricks.com/session/optimizing-apache-spark-sql-joins)
