c131cf77ab078669c1b74bdd3512a98d24a2bbed
keiji.yoshida@databricks.com <keiji.yoshida@databricks.com>
2021-02-17 08:00:57 +0000

    Update

--- Spark SQL Join Strategies/Spark SQL Join Strategies.py
+++ Spark SQL Join Strategies/Spark SQL Join Strategies.py
@@ -5,17 +5,100 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Databricks Runtime Version: 7.6 ML (includes Apache Spark 3.0.1, Scala 2.12)
-
-# COMMAND ----------
-
-# MAGIC %md
 # MAGIC ## Abstract
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC A join strategy in Spark SQL is a way to process data in a Spark cluster to perform a table join. Understanding the characteristics of each join strategy is crucial for performance tuning of Spark jobs since the peformance of a Spark job differs hugely depending on a join strategy which is used in the job. This notebook describes a list of Spark SQL join strategies and the characteristics of each join strategy.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## List of Join Strategies
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 1. Shuffle-and-replicate nested loop join
+# MAGIC 2. Sort merge join
+# MAGIC 3. Shuffle hash join
+# MAGIC 4. Broadcast hash join / broadcast nested loop join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 1. Shuffle-and-replicate Nested Loop Join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Join Hints
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Description
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 2. Sort Merge Join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Join Hints
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Description
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 3. Shuffle Hash Join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Join Hints
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Description
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 4. Broadcast Hash Join / Broadcast Nested Loop Join
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Join Hints
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Description
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Demo
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Databricks Runtime Version
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 7.6 ML (includes Apache Spark 3.0.1, Scala 2.12)
 
 # COMMAND ----------
 
